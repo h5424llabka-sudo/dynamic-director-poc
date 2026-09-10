709d3483c7327ff1c0f5cb47d45eaea7c972b9e1
c1370 <Keisuke.Azuma@nikon.com>
2026-09-10 17:56:14 +0900

    fix: correct ML Kit Subject Segmentation version from beta01 to beta1

--- dynamic-director-poc/app/build.gradle.kts
+++ dynamic-director-poc/app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("com.google.mlkit:pose-detection:17.0.0")
 
     // ML Kit Subject Segmentation
-    implementation("com.google.android.gms:play-services-mlkit-subject-segmentation:16.0.0-beta01")
+    implementation("com.google.android.gms:play-services-mlkit-subject-segmentation:16.0.0-beta1")
 
     // OpenCV
     implementation("com.quickbirdstudios:opencv:4.5.3.0")
